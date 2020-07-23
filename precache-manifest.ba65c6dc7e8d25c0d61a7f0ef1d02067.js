86d964c407b4996165fd9faebeeacbc7dc675d06
Valeriia Oliinyk <olijnykvaleria@gmail.com>
2020-07-23 21:24:21 +0300

    Updates

---
+++ precache-manifest.ba65c6dc7e8d25c0d61a7f0ef1d02067.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4333382b57e64b38c48945d4b5acb2",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "eb11a2efd71ea48cb093",
+    "url": "/goit-react-hw-06-phonebook/static/css/2.972ec59c.chunk.css"
+  },
+  {
+    "revision": "86918d6fa37ff63b4c32",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.5bb51a16.chunk.css"
+  },
+  {
+    "revision": "eb11a2efd71ea48cb093",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.1932280b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.1932280b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86918d6fa37ff63b4c32",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.e8ffe541.chunk.js"
+  },
+  {
+    "revision": "9d1a26fb56dbe9d98a08",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b0a8c527.js"
+  }
+]);
